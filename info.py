e63dcb8f475d92e856c52d26f4e1dc722a6d039d
Sergio Aguirre <sergio.m.aguirre@hotmail.com>
2025-07-21 16:33:58 -0600

    create the .env file

--- info.py
+++ info.py
@@ -5,8 +5,6 @@ datos = {"id":117,
 
 
 
-key = "F0HRZFT75QRL8N5W"
-
 url_btc = f'https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=BTC&to_currency=USD&apikey={key}'
 url_eth = f'https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=ETH&to_currency=USD&apikey={key}'
 
